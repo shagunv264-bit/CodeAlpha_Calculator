e48e709108715f86af66fe2df228d0ad637401b2
shagunv264-bit <shagunv264@gmail.com>
2026-06-04 22:37:24 +0530

    Added Calculator Project

---
+++ calculator.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    printf("===== Simple Calculator =====\n");
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case '/':
+            if(num2 != 0)
+                printf("Result = %.2f\n", num1 / num2);
+            else
+                printf("Error! Division by zero is not allowed.\n");
+            break;
+
+        default:
+            printf("Invalid Operator!\n");
+    }
+
+    return 0;
+}
